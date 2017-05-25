9965f4a89cbeafcacf9c653e8c1fde2118d16f6e
15017274 <15017274@myrp.edu.sg>
2017-05-25 12:26:48 +0800

    jj

--- app/src/main/java/com/example/a15017274/demoalarmmanager/MainActivity.java
+++ app/src/main/java/com/example/a15017274/demoalarmmanager/MainActivity.java
@@ -8,8 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
